397982bf6060def7d3328b318399f882d2bb5de6
binc876 <65497371+binc876@users.noreply.github.com>
2020-12-19 02:14:52 +0700

    Add files via upload

---
+++ app/Models/Author.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model{
+    protected $table = 'authors';
+    protected $fillable = ['name', 'gender', 'biography'];
+}
