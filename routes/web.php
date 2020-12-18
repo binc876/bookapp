d31a5a9f22c526a1972bf74259d81c905cf637b3
binc876 <65497371+binc876@users.noreply.github.com>
2020-12-19 02:20:42 +0700

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+/** @var \Laravel\Lumen\Routing\Router $router */
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It is a breeze. Simply tell Lumen the URIs it should respond to
+| and give it the Closure to call when that URI is requested.
+|
+*/
+
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
+
+$router->get('books', 'BooksController@index');
+$router->get('/books/{id}', 'BooksController@show');
+$router->post('books','BooksController@store');
+$router->put('books/{id}','BooksController@update');
+$router->delete('books/{id}','BooksController@destroy');
+
+// $router->get('/key', function(){
+//     return Str::random(32);
+// });
+
+$router->get('authors', 'AuthorController@index');
+$router->get('/authors/{id}', 'AuthorController@show');
+$router->post('authors','AuthorController@store');
+$router->put('authors/{id}','AuthorController@update');
+$router->delete('authors/{id}','AuthorController@destroy');
